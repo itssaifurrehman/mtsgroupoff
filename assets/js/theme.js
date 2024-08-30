65457df6907638a5c9a30919ba119ee3a4ce0792
Saif Ur Rehman <itssaifurrehman@gmail.com>
2024-08-30 15:01:19 +0500

    fix: offc loc animte & fav icon and offc gradient

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -152,6 +152,21 @@
     }
   });
 
+  window.addEventListener('scroll', function() {
+    var images = document.querySelectorAll('.zoom-services-item-image'); // Select all images
+    
+    images.forEach(function(image) {
+      var rect = image.getBoundingClientRect();
+      var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  
+      // Check if the image is in view
+      if (rect.top <= windowHeight && rect.bottom >= 0) {
+        image.classList.add('animate');
+      } else {
+        image.classList.remove('animate'); // Optional: remove the class if not in view
+      }
+    });
+  });
 
   $('.animsition').animsition({
     inClass: 'fade-in',
